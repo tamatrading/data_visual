d7ccfb0c1c612d7ef48cf73f409a9177243cf248
mtake <mtake88@gmail.com>
2021-10-11 21:50:33 +0900

    初版

---
+++ scatter_squares.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+
+ax.scatter(x_values, y_values,s=200)
+
+# グラフのタイトルと軸ラベルを設定する
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# 目盛りラベルのサイズを設定する
+ax.tick_params(axis="both", which="major", labelsize=14)
+
+plt.show()
