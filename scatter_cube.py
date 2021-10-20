bae4ba65158da7e8bd6b1a69cca94d2181107d49
mtake <mtake88@gmail.com>
2021-10-21 06:52:50 +0900

    108Pまで

--- scatter_cube.py
+++ scatter_cube.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 
-#x_values = [1, 2, 3, 4, 5]
-#y_values = [1, 4, 9, 16, 25]
-x_values = range(1,1001)
-y_values = [x**2 for x in x_values]
+x_values = range(1,5000)
+y_values = [x**3 for x in x_values]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
@@ -11,18 +9,18 @@ fig, ax = plt.subplots()
 ax.scatter(x_values, y_values,s=10,c=y_values,cmap=plt.cm.Blues)
 
 # グラフのタイトルと軸ラベルを設定する
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Cube Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
 
 # 目盛りラベルのサイズを設定する
 ax.tick_params(axis="both", which="major", labelsize=14)
 
 #各軸の範囲を設定する
-ax.axis([0,1100,0,1100000])
+ax.axis([0,5000,0,126_000_000_000])
 
 #グラフの保存
-plt.savefig("test.png", bbox_inches="tight")
+#plt.savefig("test.png", bbox_inches="tight")
 
 #描画
 plt.show()
