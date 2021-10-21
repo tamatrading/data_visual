de2441ca4b5e1472c225edd9f40f318a91ce3d10
tama <mtake88@gmail.com>
2021-10-21 16:49:15 +0900

    117p上まで

--- rw_visual.py
+++ rw_visual.py
@@ -4,13 +4,23 @@ from random_walk import RandomWalk
 
 while True:
     #ランダムウォークを作成する
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     #ランダムウォークの点を描画する
     plt.style.use("classic")
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values,rw.y_values,s=15)
+    fig, ax = plt.subplots(figsize=(12,7))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values,rw.y_values,s=1, c=point_numbers, cmap=plt.cm.Reds, edgecolors="none")
+
+    # 開始点と終了点を強調する
+    ax.scatter(0,0,c="green", edgecolors="none", s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+
+    #軸を削除する
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("別のランダムウォークを生成する？(y/n):")
