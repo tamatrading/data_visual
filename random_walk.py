cd0672d452969c341051d92bdb43505a5f9ebe49
mtake <mtake88@gmail.com>
2021-10-21 20:21:01 +0900

    やってみよう4-4リファクタリングまで

--- random_walk.py
+++ random_walk.py
@@ -17,13 +17,8 @@ class RandomWalk:
         # ステップ数が指定した数になるまでランダムウォークを続ける
         while len(self.x_values) < self.num_points:
             # 移動する方向と距離を決定する
-            x_direction = choice([1,-1])
-            x_distance = choice([0,1,2,3,4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1,-1])
-            y_distance = choice([0,1,2,3,4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # どこにも移動しない場合は結果を破棄する
             if x_step == 0 and y_step == 0:
@@ -35,3 +30,9 @@ class RandomWalk:
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def get_step(self):
+        """ 移動する方向と距離を計算する """
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
--- rw_visual.py
+++ rw_visual.py
@@ -4,14 +4,14 @@ from random_walk import RandomWalk
 
 while True:
     #ランダムウォークを作成する
-    rw = RandomWalk(5000)
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     #ランダムウォークの点を描画する
     plt.style.use("classic")
     fig, ax = plt.subplots(figsize=(12,7))
     point_numbers = range(rw.num_points)
-    ax.plot(rw.x_values,rw.y_values,linewidth=1,c="red")
+    ax.plot(rw.x_values,rw.y_values,linewidth=1,c="black")
 
     # 開始点と終了点を強調する
     ax.scatter(0,0,c="green", edgecolors="none", s=100)
