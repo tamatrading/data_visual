fdca9fe6b2650f8b96db31596b7f8eab36fb21df
tama <mtake88@gmail.com>
2021-10-22 14:51:37 +0900

    図4-14まで

--- dice_visual.py
+++ dice_visual.py
@@ -5,11 +5,11 @@ from die import Die
 
 #2個のD6サイコロを作成する
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 #サイコロを転がし、結果をリストに格納する
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -29,5 +29,5 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {"title":"結果", "dtick":1}
 y_axis_config = {"title":"発生した回数"}
-my_layout = Layout(title="2個の6面サイコロを1000回転がした結果",xaxis=x_axis_config,yaxis=y_axis_config)
-offline.plot({"data":data, "layout":my_layout}, filename="d6_d6.html")
+my_layout = Layout(title="6面のサイコロと10面のサイコロを50000回転がした結果",xaxis=x_axis_config,yaxis=y_axis_config)
+offline.plot({"data":data, "layout":my_layout}, filename="d6_d10.html")
